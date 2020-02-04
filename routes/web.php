07c34907fcfde3c1c3a13124b9cda56321a41845
Philip <philip_angelin@hotmail.com>
2020-02-04 21:53:30 +0100

    web.php strukturering

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,21 @@
 
 Auth::routes();
 
+// Index
 Route::get('/', 'TableCategoriesController@index');
-Route::get('/post/{id}', 'PostsController@show');
-Route::get('/thread/{title}-{id}/new', 'PostsController@create');
-Route::get('/thread/{title}-{id}', 'ThreadsController@show');
-Route::get('/category/{subcategory}-{id}/new', 'ThreadsController@create');
-Route::get('/category/{subcategory}-{id}', 'TableSubcategoriesController@show');
-Route::get('/user/{id}', 'UsersController@show');
 
+// Subcategories
+Route::get('/category/{subcategory}-{id}', 'TableSubcategoriesController@show');
+
+// Posts
 Route::post('/thread/{title}-{id}/create', 'PostsController@store');
+Route::get('/thread/{title}-{id}/new', 'PostsController@create');
+Route::get('/post/{id}', 'PostsController@show');
+
+// Threads
 Route::post('/category/{subcategory}-{id}/create', 'ThreadsController@store');
+Route::get('/category/{subcategory}-{id}/new', 'ThreadsController@create');
+Route::get('/thread/{title}-{id}', 'ThreadsController@show');
+
+// Users
+Route::get('/user/{id}', 'UsersController@show');
