1e85884431d3fba7434389d7c3110c524e259ce1
Philip <philip_angelin@hotmail.com>
2020-02-07 18:37:10 +0100

    pretty_date() klar

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -20,20 +20,38 @@ if (!function_exists('logged_in')) {
 
 if (!function_exists('is_role')) {
 	function is_role($role) {
-		if (logged_in()) return (strtolower(auth()->user()->role) === strtolower($role)) ? true : abort(403, __('Unauthorized'));
+		return (logged_in()
+			? (strtolower(auth()->user()->role) === strtolower($role)) ? true : abort(403, __('Unauthorized'))
+			: false
+		);
+	}
+}
+
+if (!function_exists('time_difference')) {
+	function time_difference($date) {
+		return $difference = [
+			'sec' => date('s', strtotime($date)) - date('s'),
+			'min' => date('i', strtotime($date)) - date('i'),
+			'hour' => date('H', strtotime($date)) - date('H'),
+			'day' => date('j', strtotime($date)) - date('j'),
+			'week' => date('W', strtotime($date)) - date('W'),
+			'month' => date('n', strtotime($date)) - date('n'),
+			'year' => date('Y', strtotime($date)) - date('Y'),
+		];
 	}
 }
 
 if (!function_exists('prettydate')) {
 	function pretty_date($date) {
-		$difference = [
-			'sec' => date('s', strtotime($date)) - date('s'),
-			'min' => date('i', strtotime($date)) - date('i'),
-			'hour' => date('H', strtotime($date)) - date('H'),
-			'day' => date('d', strtotime($date)) - date('d'),
-			'week' => date('m', strtotime($date)) - date('m'),
-			'year' => date('y', strtotime($date)) - date('y'),
-		];
-		return date('H:i') . ' ' . date('H:i', strtotime($date));
+		$diff = time_difference($date);
+		$date = strtotime($date);
+
+		if ($diff['day'] === 0 && $diff['month'] === 0 && $diff['week'] === 0 && $diff['year'] === 0) {
+			return __('Today') . ' ' . '<span class="color-gray">' . date('H:i', $date) . '</span>';
+		} else if ($diff['day'] === -1 && $diff['month'] === 0 && $diff['week'] === 0 && $diff['year'] === 0) {
+			return __('Yesterday') . ' ' . '<span class="color-gray">' . date('H:i', $date) . '</span>';
+		} else {
+			return date('Y-m-d', $date);
+		}
 	}
 }
