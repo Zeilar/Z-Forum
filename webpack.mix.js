efc85adedbe2d22471f5585130dc52875680c559
Philip <philip_angelin@hotmail.com>
2020-02-12 11:36:07 +0100

    Deploy to production script

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,17 @@ const mix = require('laravel-mix');
 
 mix.react('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+// Copy into dist folder for production
+if (mix.inProduction()) {
+	// Folders
+	mix.copyDirectory('app', 'dist/miss_hosting/zforum/app');
+	mix.copyDirectory('bootstrap', 'dist/miss_hosting/zforum/bootstrap');
+	mix.copyDirectory('config', 'dist/miss_hosting/zforum/config');
+	mix.copyDirectory('database', 'dist/miss_hosting/zforum/database');
+	mix.copyDirectory('public', 'dist/miss_hosting/public_html');
+	mix.copyDirectory('resources', 'dist/miss_hosting/zforum/resources');
+	mix.copyDirectory('routes', 'dist/miss_hosting/zforum/routes');
+	mix.copyDirectory('storage', 'dist/miss_hosting/zforum/storage');
+	mix.copyDirectory('tests', 'dist/miss_hosting/zforum/tests');
+}
