f83c3c3cf64a0c4edd619ac80495a90682c2568e
Philip <philip_angelin@hotmail.com>
2020-02-13 04:58:41 +0100

    npm run watch buggfix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,10 @@ const mix = require('laravel-mix');
  */
 
 mix.react('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .options({
+	   processCssUrls: false
+   });
 
 // Copy into dist folder for production
 if (mix.inProduction()) {
