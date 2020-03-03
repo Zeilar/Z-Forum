abda48f2ab238084f96c766b77bae546b65d2e6e
Philip <philip_angelin@hotmail.com>
2020-03-03 13:43:52 +0100

    Mer styling på <a>, selection, posts

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -15,43 +15,36 @@
 					@endif
 				@endif
 			">
-				@isset($i)
-					<span class="post-i">{{ $i }}</span>
-				@endisset
-				<div class="post-link">
-					@isset ($banner_link) 
-						{{ $banner_link }}
-					@else
-						<a href="{{route('post_permalink', [$post->id])}}">
-							{{ pretty_date($post->created_at) }}
-							<i class="fas fa-link"></i>	
+				<div class="post-meta-left">
+					<p class="post-author">
+						<a href="{{route('user_show', [$post->user->id])}}">
+							{{ $post->user->username }}
 						</a>
-					@endisset
+					</p>
+					<p class="post-author-role {{ role_coloring($post->user->role) }}">
+						{{ __(ucfirst($post->user->role)) }}
+					</p>
 				</div>
+				
+				<div class="post-meta-right">
+					@isset($i)
+						<span class="post-i">#{{ $i }}</span>
+					@endisset
+					<div class="post-link">
+						@isset ($banner_link) 
+							{{ $banner_link }}
+						@else
+							<a href="{{route('post_permalink', [$post->id])}}">
+								{{ pretty_date($post->created_at) }}
+								<i class="fas fa-link"></i>	
+							</a>
+						@endisset
+					</div>
+				</div> {{-- .post-meta-text --}}
 			</div>
 		</div>
-	</div>
+	</div> {{-- .post-header --}}
 	<div class="post-body">
 		{!! $post->content !!}
 	</div>
 </article>
-
-	{{-- <div class="post-body d-flex flex-row">
-		<div class="col user-meta">
-			<p class="user-link">
-				<a href="{{route('user_show', [$post->user->username])}}">
-					{{ $post->user->username }}
-				</a>
-			</p>
-			<p class="user-role">{{ __(ucfirst($post->user->role)) }}</p>
-			<div class="post-avatar w-50">
-				<img class="img-fluid" src="/storage/user-avatars/{{$post->user->avatar}}" />
-			</div>
-			<p class="user-date">{{ __('Registered: ' . date('M Y', strtotime($post->user->created_at))) }}</p>
-		</div>
-		<div class="post-content">
-			{!! $post->content !!}
-		</div>
-	</div>
-	<i class="fas fa-link"></i>
-	 --}}
