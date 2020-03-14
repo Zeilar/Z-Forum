5c162242950f39c058729dbc4d1809028f242ca4
Philip <philip_angelin@hotmail.com>
2020-03-14 04:57:11 +0100

    Ändrat table seed så user posts är mer utspridda

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,25 +12,24 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        // Posts by user 1 (Superadmin)
 		Post::create([
-			'content' 	=> 'This expansion is the worst',
+			'content' => 'This expansion is the worst',
 			'thread_id' => 1,
-			'user_id' 	=> 1,
+			'user_id' => 1,
 			'table_subcategory_id' => 1,
 			'table_category_id' => 1,
 		]);
 		Post::create([
 			'content' => 'Agreed, I loved how scary the reaper leviathans were!',
 			'thread_id' => 2,
-			'user_id' => 1,
+			'user_id' => 2,
 			'table_subcategory_id' => 2,
 			'table_category_id' => 1,
 		]);
 		Post::create([
 			'content' => 'Don\'t bully pourpol :(',
 			'thread_id' => 3,
-			'user_id' => 1,
+			'user_id' => 3,
 			'table_subcategory_id' => 3,
 			'table_category_id' => 1,
 		]);
@@ -44,23 +43,21 @@ class PostsTableSeeder extends Seeder
 		Post::create([
 			'content' => 'It\'s been a long day without you my friend...',
 			'thread_id' => 5,
-			'user_id' => 1,
+			'user_id' => 2,
 			'table_subcategory_id' => 5,
 			'table_category_id' => 1,
 		]);
-
-		// Threads by user 2 (Moderator)
 		Post::create([
 			'content' => 'But can it run Crysis 2???',
 			'thread_id' => 6,
-			'user_id' => 2,
+			'user_id' => 3,
 			'table_subcategory_id' => 6,
 			'table_category_id' => 2,
 		]);
 		Post::create([
 			'content' => 'Because NvidiaTM',
 			'thread_id' => 7,
-			'user_id' => 2,
+			'user_id' => 1,
 			'table_subcategory_id' => 7,
 			'table_category_id' => 2,
 		]);
@@ -74,23 +71,21 @@ class PostsTableSeeder extends Seeder
 		Post::create([
 			'content' => 'The future is now old man',
 			'thread_id' => 9,
-			'user_id' => 2,
+			'user_id' => 3,
 			'table_subcategory_id' => 9,
 			'table_category_id' => 2,
 		]);
 		Post::create([
 			'content' => 'Did you try turning it off and on again?',
 			'thread_id' => 10,
-			'user_id' => 2,
+			'user_id' => 1,
 			'table_subcategory_id' => 10,
 			'table_category_id' => 2,
 		]);
-
-		// Threads by user 3 (User)
 		Post::create([
 			'content' => 'God Emperor Trump brother',
 			'thread_id' => 11,
-			'user_id' => 3,
+			'user_id' => 2,
 			'table_subcategory_id' => 11,
 			'table_category_id' => 3,
 		]);
@@ -104,14 +99,14 @@ class PostsTableSeeder extends Seeder
 		Post::create([
 			'content' => 'Yes.',
 			'thread_id' => 13,
-			'user_id' => 3,
+			'user_id' => 1,
 			'table_subcategory_id' => 13,
 			'table_category_id' => 3,
 		]);
 		Post::create([
 			'content' => 'Still looks better than your mom',
 			'thread_id' => 14,
-			'user_id' => 3,
+			'user_id' => 2,
 			'table_subcategory_id' => 14,
 			'table_category_id' => 3,
 		]);
--- database/seeds/ThreadsTableSeeder.php
+++ database/seeds/ThreadsTableSeeder.php
@@ -12,7 +12,6 @@ class ThreadsTableSeeder extends Seeder
      */
     public function run()
     {
-		// Threads by user 1 (Superadmin)
         Thread::create([
 			'title' => '8.3 sucks!',
 			'slug' => urlencode('8.3 sucks!'),
@@ -23,14 +22,14 @@ class ThreadsTableSeeder extends Seeder
 		Thread::create([
 			'title' => 'Subnautica is the best underwater game ever',
 			'slug' => urlencode('Subnautica is the best underwater game ever'),
-			'user_id' => 1,
+			'user_id' => 2,
 			'table_subcategory_id' => 2,
 			'table_category_id' => 1,
 		]);
 		Thread::create([
 			'title' => 'Faking pourpol again!',
 			'slug' => urlencode('Faking pourpol again!'),
-			'user_id' => 1,
+			'user_id' => 3,
 			'table_subcategory_id' => 3,
 			'table_category_id' => 1,
 		]);
@@ -44,23 +43,21 @@ class ThreadsTableSeeder extends Seeder
 		Thread::create([
 			'title' => 'RIP Terraria 2011-2020, good night sweet prince',
 			'slug' => urlencode('RIP Terraria 2011-2020, good night sweet prince'),
-			'user_id' => 1,
+			'user_id' => 2,
 			'table_subcategory_id' => 5,
 			'table_category_id' => 1,
 		]);
-
-		// Threads by user 2 (Moderator)
 		Thread::create([
 			'title' => 'AMD Threadripper 3990X can run Crysis',
 			'slug' => urlencode('AMD Threadripper 3990X can run Crysis'),
-			'user_id' => 2,
+			'user_id' => 3,
 			'table_subcategory_id' => 6,
 			'table_category_id' => 2,
 		]);
 		Thread::create([
 			'title' => 'Why is GTX 1060 getting GDDR6?',
 			'slug' => urlencode('Why is GTX 1060 getting GDDR6?'),
-			'user_id' => 2,
+			'user_id' => 1,
 			'table_subcategory_id' => 7,
 			'table_category_id' => 2,
 		]);
@@ -74,23 +71,21 @@ class ThreadsTableSeeder extends Seeder
 		Thread::create([
 			'title' => 'M.2 is the future',
 			'slug' => urlencode('M.2 is the future'),
-			'user_id' => 2,
+			'user_id' => 3,
 			'table_subcategory_id' => 9,
 			'table_category_id' => 2,
 		]);
 		Thread::create([
 			'title' => 'Corsair CX300 blew up? House is on fire, what do?',
 			'slug' => urlencode('Corsair CX300 blew up? House is on fire, what do?'),
-			'user_id' => 2,
+			'user_id' => 1,
 			'table_subcategory_id' => 10,
 			'table_category_id' => 2,
 		]);
-
-		// Threads by user 3 (User)
 		Thread::create([
 			'title' => 'Trump 2020 hell ye brother',
 			'slug' => urlencode('Trump 2020, hell ye brother'),
-			'user_id' => 3,
+			'user_id' => 2,
 			'table_subcategory_id' => 11,
 			'table_category_id' => 3,
 		]);
@@ -104,14 +99,14 @@ class ThreadsTableSeeder extends Seeder
 		Thread::create([
 			'title' => 'ywoepafnyawi8yvf',
 			'slug' => urlencode('ywoepafnyawi8yvf'),
-			'user_id' => 3,
+			'user_id' => 1,
 			'table_subcategory_id' => 13,
 			'table_category_id' => 3,
 		]);
 		Thread::create([
 			'title' => 'Nice dingo pictures',
 			'slug' => urlencode('Nice dingo pictures'),
-			'user_id' => 3,
+			'user_id' => 2,
 			'table_subcategory_id' => 14,
 			'table_category_id' => 3,
 		]);
