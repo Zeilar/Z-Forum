2a6269e6136ca1ab7b7d63400927e1dcc92316dd
Philip <philip_angelin@hotmail.com>
2020-03-29 04:34:25 +0200

    Buggfix så databas funkar mellan 02-03 GMT
    ???????????????????

--- config/database.php
+++ config/database.php
@@ -45,6 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+			'timezone' => '+00:00',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
