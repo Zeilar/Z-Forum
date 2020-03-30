e354ac71fd168eee16bbab10c07563f38dc0cdd7
Philip <philip_angelin@hotmail.com>
2020-03-30 20:25:55 +0200

    Rensat JS lite

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,7 +11,6 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
-	require('wysibb');
 } catch (e) {}
 
 /**
@@ -24,19 +23,9 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo';
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     encrypted: true
-// });
+// TinyMCE default values
+tinymce.init({
+	selector: '#form-content',
+	plugins: "bbcode",
+	bbcode_dialect: "punbb"
+});
