5e86ae118aec12eea5b0cdb106a6f55be94e0a3d
Philip <philip_angelin@hotmail.com>
2020-04-05 18:43:38 +0200

    Lagt till testing så massor användare är online konstant

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use \Carbon\Carbon;
 use App\Post;
+use App\User;
+use \Cache;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +28,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-		
+		// FOR TESTING PURPOSES ONLY, REMOVE IN PRODUCTION
+		$users = User::where('id', '!=', '1')->get();
+		$users = $users->chunk(ceil($users->count() / 2))[0];
+
+		foreach ($users as $user) {
+			$expiresAt = Carbon::now()->addYear(1);
+			Cache::put('user-online-' . $user->id, true, $expiresAt);
+		}
     }
 }
