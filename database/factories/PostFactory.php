ca06b6bf043baecca324f07e0314d2c848f78919
Philip <philip_angelin@hotmail.com>
2020-04-05 14:20:11 +0200

    Fixat post factory så första post alltid är OP

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -11,11 +11,24 @@ use App\User;
 
 $factory->define(Post::class, function (Faker $faker) {
 	$thread = Thread::all()->random();
+	$user = User::all()->random();
 	$date = Carbon::now()->subSeconds(rand(0, DAY_IN_SECONDS * 3));
+
+	if (!count($thread->posts) && $user->id !== $thread->user->id) {
+		Post::create([
+			'content' 		 => $faker->realText(rand(50, 400), rand(1, 4)),
+			'user_id' 		 => $thread->user->id,
+			'thread_id' 	 => $thread->id,
+			'subcategory_id' => $thread->subcategory->id,
+			'category_id' 	 => $thread->subcategory->category->id,
+			'created_at'	 => Carbon::now()->subDays(4),
+			'updated_at'	 => Carbon::now()->subDays(4),
+		]);
+	}
 	
     return [
         'content' 		 => $faker->realText(rand(50, 400), rand(1, 4)),
-		'user_id' 		 => User::all()->random()->id,
+		'user_id' 		 => $user->id,
 		'thread_id' 	 => $thread->id,
 		'subcategory_id' => $thread->subcategory->id,
 		'category_id' 	 => $thread->subcategory->category->id,
