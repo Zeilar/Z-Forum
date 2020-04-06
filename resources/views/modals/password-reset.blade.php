597f9cec4dd7b19f528a5cda363b427bea2b4d52
Philip <philip_angelin@hotmail.com>
2020-04-06 12:43:45 +0200

    Password reset modal

---
+++ resources/views/modals/password-reset.blade.php
@@ -0,0 +1,32 @@
+<div id="passwordresetModal" class="modal fade">
+	<div class="modal-dialog modal-auth">
+		<div class="modal-content">
+			<form method="POST" action="{{route('password_reset_sender')}}">
+				@csrf
+				<div class="modal-header">				
+					<h4 class="modal-title">{{ __('Reset password') }}</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				</div>
+				<div class="modal-body">				
+					<div class="form-group">
+						<label>{{ __('Email') }}</label>
+						@error('email') <p class="color-red" id="error-email">{{ $message }}</p> @enderror
+						<input type="email" id="register_email" name="email" 
+							class="form-control @error('email') is-invalid @enderror" autofocus
+							value="{{old('email')}}" placeholder="john.doe@gmail.com"
+						/>
+					</div>
+				</div>
+				<div class="modal-footer">		
+					<button type="submit" class="btn spin rounded btn-block btn-success" disabled>
+						<span>{{__('Reset')}}</span>
+					</button>
+					<p>
+						{{ __('Already a member?') }}
+						<a data-toggle="modal" data-dismiss="modal" href="#loginModal">{{ __('Sign in') }}</a>
+					</p>
+				</div>
+			</form>
+		</div>
+	</div>
+</div> 
