dc869af5996ecb2d26218f3e8eebdb9c60a27ae4
Philip <philip_angelin@hotmail.com>
2020-04-20 09:19:11 +0200

    Justerade post banner i profile

--- resources/views/user/posts.blade.php
+++ resources/views/user/posts.blade.php
@@ -10,7 +10,19 @@
 			<div class="profile-posts">
 				@foreach ($posts as $post)
 					@component('components.post', ['post' => $post, 'disablePostToolbar' => true])
-						
+						@slot('banner_link')
+							<a href="{{
+								route('post_show', [
+									$post->thread->id,
+									$post->thread->slug,
+									get_item_page_number($post->thread->posts->sortBy('created_at'), $post->id, settings_get('posts_per_page')),
+									$post->id,
+								])
+							}}">
+								{{ __('View in thread') }}
+								<i class="fas fa-sign-in-alt"></i>
+							</a>
+						@endslot
 					@endcomponent
 				@endforeach
 			</div>
