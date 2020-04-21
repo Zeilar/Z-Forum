7686152b17934b1354b82598dce8934a36c33afe
Philip <philip_angelin@hotmail.com>
2020-04-21 09:44:56 +0200

    Settings WIP

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,23 +13,10 @@ use App\{
 
 class DashboardController extends Controller
 {
-	public function superadmin()
-	{
-		if (logged_in()) {
-			if (is_role('superadmin')) {
-				return view('dashboard.superadmin');
-			} else {
-				return msg_error('role');
-			}
-		} else {
-			return msg_error('login');
-		}
-	}
-
 	public function account()
 	{
 		if (logged_in()) {
-			return view('dashboard.account');
+			return view('dashboard.account', ['user' => auth()->user()]);
 		} else {
 			return msg_error('login');
 		}
--- resources/views/dashboard/account.blade.php
+++ resources/views/dashboard/account.blade.php
@@ -1,21 +1,30 @@
 @extends('head')
 
 @section('pageTitle')
-	{{ __('Account') }}
+	{{ __('Settings') }}
 @endsection
 
 @section('content')
 	<div id="settings">
 		<form action="{{route('dashboard_account_update')}}" method="post" enctype="multipart/form-data">
 			@csrf
+			<input type="hidden" name="_method" value="PUT" />
+
 			<div class="form-group">
-				<input type="hidden" name="_method" value="PUT" />
+				<p>{{ __('Avatar') }}</p>
+
 				<label class="file-upload" for="avatar-upload">
 					<i class="fas color-white fa-upload"></i>
-					<span>{{ __('Choose a file') }}</span>
+					<span>{{ __('Upload file') }}</span>
 				</label>
 				<input type="file" id="avatar-upload" name="user-avatar" />
 			</div>
+
+			<div class="form-group">
+				<label for="settings-email">Email address</label>
+    			<input type="email" class="form-control" id="settings-email" placeholder="{{$user->email}}">
+			</div>
+
 			<button type="submit">Submit</button>
 		</form>
 	</div>
