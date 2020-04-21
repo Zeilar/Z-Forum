e24838b72f63218ca852e50f18e0e7b9748cf139
Philip <philip_angelin@hotmail.com>
2020-04-21 16:59:22 +0200

    Små felhanteringar på profile

--- resources/views/user/posts.blade.php
+++ resources/views/user/posts.blade.php
@@ -8,23 +8,27 @@
 	@component('components.profile', ['user' => $user, 'posts_with_likes' => $posts_with_likes, 'active' => 'posts'])
 		@slot('posts')
 			<div class="profile-lower posts">
-				@foreach ($posts as $post)
-					@component('components.post', ['post' => $post, 'disablePostToolbar' => true])
-						@slot('banner_link')
-							<a href="{{
-								route('post_show', [
-									$post->thread->id,
-									$post->thread->slug,
-									get_item_page_number($post->thread->posts->sortBy('created_at'), $post->id, settings_get('posts_per_page')),
-									$post->id,
-								])
-							}}">
-								{{ __('View in thread') }}
-								<i class="fas fa-sign-in-alt"></i>
-							</a>
-						@endslot
-					@endcomponent
-				@endforeach
+				@if (count($posts))	
+					@foreach ($posts as $post)
+						@component('components.post', ['post' => $post, 'disablePostToolbar' => true])
+							@slot('banner_link')
+								<a href="{{
+									route('post_show', [
+										$post->thread->id,
+										$post->thread->slug,
+										get_item_page_number($post->thread->posts->sortBy('created_at'), $post->id, settings_get('posts_per_page')),
+										$post->id,
+									])
+								}}">
+									{{ __('View in thread') }}
+									<i class="fas fa-sign-in-alt"></i>
+								</a>
+							@endslot
+						@endcomponent
+					@endforeach
+				@else
+					{{ __('No posts were found 🤔') }}
+				@endif
 			</div>
 		@endslot
 
