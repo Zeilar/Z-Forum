32174ee9159e9afb0764943ab32fec120316067a
Philip <philip_angelin@hotmail.com>
2020-04-27 22:45:57 +0200

    Bytt plats på new message subject/recipient

--- resources/views/message/new.blade.php
+++ resources/views/message/new.blade.php
@@ -16,6 +16,14 @@
 	<form action="{{route('message_send')}}" method="post">
 		@csrf
 		<div class="message-form">
+			<fieldset class="message-form-recipient">
+				<legend>
+					<h4>{{ __('Send to') }}</h4>
+				</legend>
+				@error('recipient') <p style="color: red;">{{ $message }}</p> @enderror
+				<input type="text" name="recipient" id="message-recipient" value="{{ request()->query('replyTo') ?? old('recipient') }}" />
+			</fieldset>
+
 			<fieldset class="message-form-title">
 				<legend>
 					<h4>{{ __('Subject') }}</h4>
@@ -25,14 +33,6 @@
 					@php $replySubject = 'RE: ' . request()->query('replySubject') @endphp
 				@endif
 				<input type="text" name="title" id="message-title" value="{{ $replySubject ?? old('title') }}" />
-			</fieldset>
-
-			<fieldset class="message-form-recipient">
-				<legend>
-					<h4>{{ __('Send to') }}</h4>
-				</legend>
-				@error('recipient') <p style="color: red;">{{ $message }}</p> @enderror
-				<input type="text" name="recipient" id="message-recipient" value="{{ request()->query('replyTo') ?? old('recipient') }}" />
 			</fieldset>
 
 			<fieldset class="message-form-content">
