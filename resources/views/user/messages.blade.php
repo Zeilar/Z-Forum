96807df9d8e76b1e258cdfb7dab9262182eca341
Philip <philip_angelin@hotmail.com>
2020-05-02 23:50:01 +0200

    Buggfix när messages är tomt

--- resources/views/user/messages.blade.php
+++ resources/views/user/messages.blade.php
@@ -17,14 +17,10 @@
                 @else
                     <p>{{ __('No messages were found 👻') }}</p>
 
-                    @if ($message->author->id !== auth()->user()->id)
-                        <a class="btn mt-2 btn-success-full" href="{{
-                            route('message_new') . '?replyTo=' . $user->username
-                        }}">
-                            <span>{{ __('Send a message') }}</span>
-                            <i class="fas ml-2 color-white fa-reply"></i>
-                        </a>
-                    @endif
+                    <a class="btn mt-2 btn-success-full" href="{{route('message_new')}}">
+                        <span>{{ __('Send a message') }}</span>
+                        <i class="fas ml-2 color-white fa-reply"></i>
+                    </a>
                 @endif
             </div>
 		@endslot
