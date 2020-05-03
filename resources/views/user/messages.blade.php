51f37449fa69d48af2e9cfbaa1d12f1522f88f9f
Philip <philip_angelin@hotmail.com>
2020-05-03 15:58:24 +0200

    Fixat profil text i mobil samt småfix

--- resources/views/user/messages.blade.php
+++ resources/views/user/messages.blade.php
@@ -17,7 +17,7 @@
                 @else
                     <p>{{ __('No messages were found 👻') }}</p>
 
-                    <a class="btn mt-2 btn-success-full" href="{{route('message_new')}}">
+                    <a class="btn mt-2 btn-success-full" href="{{ route('message_new') . '?replyTo=' . $user->username }}">
                         <span>{{ __('Send a message') }}</span>
                         <i class="fas ml-2 color-white fa-reply"></i>
                     </a>
