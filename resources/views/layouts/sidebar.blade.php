d0f6a5b09e8f01156284507e02e8c560793c7d52
Philip <philip_angelin@hotmail.com>
2020-05-07 23:28:29 +0200

    parseEmote POGGERS

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -159,6 +159,8 @@
                                         message.find('.content').text(`${response.message.content}`);
                                         $('.chat-box').scrollTop($('.chat-box')[0].scrollHeight);
                                         initChatMessageRemoveHandlers();
+
+                                        parseEmotes(message.find('.content'));
                                     }                            
                                 },
                                 error: function(error) {
@@ -167,6 +169,41 @@
                             });
                         }
                     }, 500);
+
+                    async function parseEmotes(message) {
+                        let s = message.html();
+                        let emotes = s.match(/:([a-zA-Z]+):/g);
+
+                        if (emotes != null && emotes.length) {
+                            let trimmed = [];
+                            let parsed = [];
+
+                            emotes.forEach(element => {
+                                trimmed.push(element.replace(/:/g, ''));
+                            });
+
+                            for (let i = 0; i < trimmed.length; i++) {
+                                await fetch('/storage/emoticons/' + trimmed[i] + '.png').then(response => {
+                                    if (response.status === 200) {
+                                        parsed.push(emotes[i]);
+                                    }
+                                });
+                            }
+
+                            for (let i = 0; i < parsed.length; i++) {
+                                let path = parsed[i].replace(/:/g, '');
+                                s = s.replace(parsed[i], `<img src="/storage/emoticons/${path}.png">`);
+                            }
+                            return message.html(s);
+                        } else {
+                            return false;
+                        }
+                    }
+
+                    $('.chat-message .content').each(function() {
+                        parseEmotes($(this));
+                    })
+                    
                 </script>
 
                 @auth
@@ -216,6 +253,8 @@
                                         $('#chat-content').removeClass('error');
                                         $('#chat-content').val('').focus();
                                         initChatMessageRemoveHandlers();
+
+                                        parseEmotes(message.find('.content'));
                                     }
                                 },
                                 error: function(error) {
