db862a817f1691b6bb069f66a6058f2544c1e38b
Philip <philip_angelin@hotmail.com>
2020-05-09 08:56:52 +0200

    Buggfix om toolbarItemOpen cookie var undefined

--- resources/views/components/toolbar-item.blade.php
+++ resources/views/components/toolbar-item.blade.php
@@ -1,11 +1,15 @@
+@if (isset($cookie) && isset($_COOKIE['toolbarItemOpen']) && $_COOKIE['toolbarItemOpen'] === $cookie)
+    @php $match = true @endphp
+@endif
+
 <div class="toolbar-row">
-    <div class="toolbar-icon @if($cookie !== null && $_COOKIE['toolbarItemOpen'] === $cookie) active @endif">
+    <div class="toolbar-icon @isset($match) active @endisset">
         @isset($icon)
             {!! $icon !!}
         @endisset
         <h5 class="toolbar-category" data-cookie="{{ $cookie ?? '' }}">{{ $categoryTitle ?? '' }}</h5>
     </div>
-    <div class="toolbar-item @if($cookie !== null && $_COOKIE['toolbarItemOpen'] === $cookie) show @endif">
+    <div class="toolbar-item @isset($match) show @endisset">
         @isset($toolbarSubitem)
             {{ $toolbarSubitem }}
         @endisset
