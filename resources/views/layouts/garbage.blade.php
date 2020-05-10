6bd6c967aaaf7493a3b6b679bf9d83979933c6c3
Philip <philip_angelin@hotmail.com>
2020-05-10 02:45:28 +0200

    Garbage styling och felhantering

--- resources/views/layouts/garbage.blade.php
+++ resources/views/layouts/garbage.blade.php
@@ -6,6 +6,10 @@
 
 @section('content')
     <div id="garbage">
+        @if (!count($subcategories) && !count($chat_messages) && !count($user_messages) && !count($categories) && !count($threads) && !count($posts))
+            <h2 class="garbage-empty">{{ __('The garbage can is empty') }}</h2>
+        @endif
+    
         @foreach($posts as $post)
             @component('components.post', [
                 'post'				   => $post,
